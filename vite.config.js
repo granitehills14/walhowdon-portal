08ea4e2ae9a07c9e579a3cf995e4d9bdf576595b
Dominic Filiano <dominic.l.filiano.14@gmail.com>
2025-06-14 10:15:21 -0400

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/granitehills14/walhowdon-portal' // Replace with your GitHub repo name
+  base: 'https://github.com/granitehills14/walhowdon-portal'
 });
