075d52053b34a83339557c490c788ba8de4322f6
Dominic Filiano <dominic.l.filiano.14@gmail.com>
2025-06-21 14:43:48 -0400

    updated to static html

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import WalhowdonViewerDashboard from './WalhowdonViewerDashboard';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <WalhowdonViewerDashboard />
-  </React.StrictMode>
-);
